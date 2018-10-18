ce857537b497c0f65a9d1e1e6964f08ccc6488bb
Nivrith <nivrith.mandayam@gmail.com>
2018-10-18 23:10:52 +1030

    Add Type Declarations

---
+++ lib/insta.d.ts
@@ -0,0 +1,5 @@
+declare class Insta {
+    getReport(username?: string): Promise<void>;
+}
+declare const _default: Insta;
+export default _default;
---
+++ lib/socialy.d.ts
@@ -0,0 +1,9 @@
+declare class Socialy {
+    twitter(username?: string): Promise<any>;
+    /**
+     * instagram
+     */
+    instagram(username?: string): Promise<void>;
+}
+declare const _default: Socialy;
+export default _default;
